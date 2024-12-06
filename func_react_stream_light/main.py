2f50585e804c984f8b02b7fb51bfec6b6a71a89d
finddme <53667002+finddme@users.noreply.github.com>
2024-12-06 16:06:18 +0900

    Update main.py

--- func_react_stream_light/main.py
+++ func_react_stream_light/main.py
@@ -38,6 +38,24 @@ os.system("""sudo apt-get install -y \
                                         fonts-wqy-zenhei \
                                         fonts-tlwg-loma-otf \
                                         fonts-freefont-ttf""")
+os.system("""sudo apt-get install libnss3\
+            libnspr4\
+            libatk1.0-0\
+            libatk-bridge2.0-0\
+            libcups2\
+            libdrm2\
+            libxcomposite1\
+            libxdamage1\ 
+            libxfixes3\
+            libxrandr2\
+            libgbm1\
+            libxkbcommon0\
+            libpango-1.0-0\
+            libcairo2\
+            libasound2\
+            libatspi2.0-0 
+            """)
+
 import argparse
 import six, torch
 from app.direct_streamlit import streamlit_app
