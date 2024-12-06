3b44d4edc4f0233a645716a478791a534fef17c0
finddme <53667002+finddme@users.noreply.github.com>
2024-12-06 15:24:48 +0900

    Update main.py

--- func_react_stream_light/main.py
+++ func_react_stream_light/main.py
@@ -1,6 +1,6 @@
 import os
 os.system("playwright install")
-os.system("apt-get install libnss3\libnspr4\libatk1.0-0\libatk-bridge2.0-0\libcups2\libdrm2\libxkbcommon0\libatspi2.0-0\libxcomposite1\libxdamage1\libxfixes3\libxrandr2\libgbm1\libpango-1.0-0\libcairo2\libasound2")
+os.system("sudo apt-get install libnss3\libnspr4\libatk1.0-0\libatk-bridge2.0-0\libcups2\libdrm2\libxkbcommon0\libatspi2.0-0\libxcomposite1\libxdamage1\libxfixes3\libxrandr2\libgbm1\libpango-1.0-0\libcairo2\libasound2")
 import argparse
 import six, torch
 from app.direct_streamlit import streamlit_app
