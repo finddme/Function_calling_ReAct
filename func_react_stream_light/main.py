e70e00f951e93598e415221873b09d1208312ff0
finddme <53667002+finddme@users.noreply.github.com>
2024-12-06 15:54:44 +0900

    Update main.py

--- func_react_stream_light/main.py
+++ func_react_stream_light/main.py
@@ -1,6 +1,43 @@
 import os
 os.system("playwright install")
-os.system("sudo playwright install-deps")
+os.system("""sudo apt-get install -y \
+                                        libwoff1 \
+                                        libopus0 \
+                                        libwebp7 \
+                                        libwebpdemux2 \
+                                        libenchant-2-2 \
+                                        libgudev-1.0-0 \
+                                        libsecret-1-0 \
+                                        libhyphen0 \
+                                        libgdk-pixbuf2.0-0 \
+                                        libegl1 \
+                                        libnotify4 \
+                                        libxslt1.1 \
+                                        libevent-2.1-7 \
+                                        libgles2 \
+                                        libxcomposite1 \
+                                        libatk1.0-0 \
+                                        libatk-bridge2.0-0 \
+                                        libepoxy0 \
+                                        libgtk-3-0 \
+                                        libharfbuzz-icu0 \
+                                        libgstreamer-gl1.0-0 \
+                                        libgstreamer-plugins-bad1.0-0 \
+                                        gstreamer1.0-plugins-good \
+                                        gstreamer1.0-plugins-bad \
+                                        libxt6 \
+                                        libxaw7 \
+                                        xvfb \
+                                        fonts-noto-color-emoji \
+                                        libfontconfig \
+                                        libfreetype6 \
+                                        xfonts-cyrillic \
+                                        xfonts-scalable \
+                                        fonts-liberation \
+                                        fonts-ipafont-gothic \
+                                        fonts-wqy-zenhei \
+                                        fonts-tlwg-loma-otf \
+                                        fonts-freefont-ttf""")
 import argparse
 import six, torch
 from app.direct_streamlit import streamlit_app
