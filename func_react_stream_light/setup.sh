97360488fd2d96ef8a1f01a8cca9547a8788f24b
finddme <53667002+finddme@users.noreply.github.com>
2024-12-07 15:13:47 +0900

    Update setup.sh

--- func_react_stream_light/setup.sh
+++ func_react_stream_light/setup.sh
@@ -2,4 +2,20 @@ pip install playwright
 playwright install-deps
 playwright install
 
-
+apt-get install libnss3
+apt-get install libnspr4
+apt-get install libatk1.0-0
+apt-get install libatk-bridge2.0-0
+apt-get install libcups2
+apt-get install libdrm2
+apt-get install libxcomposite1
+apt-get install libxdamage1
+apt-get install libxfixes3
+apt-get install libxrandr2
+apt-get install libgbm1
+apt-get install libxkbcommon0
+apt-get install libpango-1.0-0
+apt-get install libcairo2
+apt-get install libasound2
+apt-get install libatspi2.0-0
+apt-get install libwayland-client0
