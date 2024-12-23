f6aa0dbe3b92b6cc266f3018d1663e1c1be5a52f
finddme <53667002+finddme@users.noreply.github.com>
2024-12-23 17:25:02 +0900

    Update streamlit.py

--- func_react_stream/blog_search_box/streamlit.py
+++ func_react_stream/blog_search_box/streamlit.py
@@ -25,7 +25,7 @@ async def main():
     st.markdown(""":orange[**Law | Finance | Ai | Conversation**]""", 
                 unsafe_allow_html=True)
 
-    api_url = "https://d090-222-233-253-190.ngrok-free.app/chat"
+    api_url = " https://dfe0ef12cbdc.ngrok.app/chat"
 
     user_input = st.text_input("검색어를 입력하세요:", "")
 
